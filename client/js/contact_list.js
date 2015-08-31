f5215779c42342a9a02a95952088ecf61fe859c6
hong19 <jimmy600kimo@hotmail.com>
2015-08-31 19:38:19 +0800

    [feature] edit contact form

--- client/js/contact_list.js
+++ client/js/contact_list.js
@@ -1,13 +1,12 @@
 
 Template.contact_list_main.helpers({
     contacts: function(){
-    	return Contacts.find({});
+    	return Contacts.find({} , { sort: {createdAt: -1 }});
     }
 });
 
 Template.contact_list_main.events({
-	"submit #new-contact": function(event, template) {
-		console.log("submit");
+	"submit #new_contact_form": function(event) {
 		// Prevent default browser form submit
 		event.preventDefault();
 
@@ -17,7 +16,9 @@ Template.contact_list_main.events({
 			lastname: event.target.lastname.value,
 			phone: event.target.phone.value,
 			email: event.target.email.value,
-			birthday: event.target.birthday.value
+			birthday: event.target.birthday.value,
+			createdAt: new Date(),
+			edit_mode: false
 		};
 
 		// Insert a contact into the collection
@@ -30,5 +31,43 @@ Template.contact_list_main.events({
 		event.target.email.value = "";
 		event.target.birthday.value = "";
 		
+	},
+
+
+	"click .btn-edit": function( event ){
+		//Blaze.renderWithData( Template.edit_form, {}, event.currentTarget.parentNode.parentNode.parentNode );
+		Contacts.update( this._id,{ 
+			$set: { edit_mode:  !this.edit_mode }
+		});
+	},
+
+	"click .btn-delete": function(){
+		// _id is the identifier of object in mongoDB
+		Contacts.remove( this._id )
+	}
+
+
+});
+
+Template.contact.events({
+	"click .btn-success": function(event){
+		console.log("submit edit_contact_form");
+		console.log( this );
+		console.log( event.target );
+		// Prevent default browser form submit
+		event.preventDefault();
+
+		var form_target = $('#edit_contact_form');
+
+		Contacts.update( this._id,{
+			$set: {
+				firstname: form_target[0].firstname.value,
+				lastname: form_target[0].lastname.value,
+				phone: form_target[0].phone.value,
+				email: form_target[0].email.value,
+				birthday: form_target[0].birthday.value,
+				edit_mode: false
+			} 
+		});
 	}
 });
